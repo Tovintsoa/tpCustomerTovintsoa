aad67bd08c1e720f2b1a54a7aafea3eedfe38606
Tovintsoa <tovintsoa1337@capri-nc.com>
2023-03-17 08:56:23 +0300

    Push oublié pour execution projet

---
+++ src/main/java/mg/itu/tovintsoa/tpcustomertovintsoa/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tovintsoa.tpcustomertovintsoa.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Tovintsoa-Capri
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
